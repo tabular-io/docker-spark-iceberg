f70792c573dbdde7404ba8f10cc4e5f941ffd829
samsetegne <43911210+samredai@users.noreply.github.com>
2021-10-16 20:47:15 -0700

    movielens data and a hive metastore (postgres)

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -2,9 +2,12 @@
 
 export SPARK_DIST_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath)
 
+schematool -dbType postgres -initSchema && hive --service metastore &
+
 /opt/spark/sbin/start-master.sh -p 7077
 /opt/spark/sbin/start-worker.sh spark://spark-iceberg:7077
 
-alias pyspark-notebook="PYSPARK_DRIVER_PYTHON=jupyter-notebook PYSPARK_DRIVER_PYTHON_OPTS=\"--notebook-dir=/home/iceberg/notebooks --ip='*' --port=8888 --no-browser --allow-root\" pyspark"
+spark-sql -f /opt/spark/bootstrap/ddl.sql
+pyspark < /opt/spark/bootstrap/load_movielens_data.py
 
 tail -f /dev/null
