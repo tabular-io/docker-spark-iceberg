aa2f6887c78c44a35f902270670951562e8c9a1a
samsetegne <43911210+samredai@users.noreply.github.com>
2021-11-16 14:50:02 -0800

    JDBC Catalog, almost there

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-export SPARK_DIST_CLASSPATH=$(/opt/hadoop/bin/hadoop classpath)
-
-schematool -dbType postgres -initSchema && hive --service metastore &
-
 /opt/spark/sbin/start-master.sh -p 7077
 /opt/spark/sbin/start-worker.sh spark://spark-iceberg:7077
-
-spark-sql -f /opt/spark/bootstrap/ddl.sql
-pyspark < /opt/spark/bootstrap/load_movielens_data.py
 
 tail -f /dev/null
