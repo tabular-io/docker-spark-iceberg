fe351940a46a13d235badac245789d27100ddba4
Fokko Driesprong <fokko@tabular.io>
2022-12-06 15:52:17 +0100

    Add thrift server
    This makes it easy to run dbt against Spark

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -3,6 +3,7 @@
 start-master.sh -p 7077
 start-worker.sh spark://spark-iceberg:7077
 start-history-server.sh
+start-thriftserver.sh
 
 # Entrypoint, for example notebook, pyspark or spark-sql
 if [[ $# -gt 0 ]] ; then
