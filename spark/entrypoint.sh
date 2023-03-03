bbb45b678db630048888df79b05aed1f5b70470f
Eduard Tudenhöfner <etudenhoefner@gmail.com>
2023-03-03 12:35:44 +0100

    Use separate derby folder for thriftserver
    This is so that the spark shell can still be started while the thriftserver is running

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -20,7 +20,7 @@
 start-master.sh -p 7077
 start-worker.sh spark://spark-iceberg:7077
 start-history-server.sh
-start-thriftserver.sh
+start-thriftserver.sh  --driver-java-options "-Dderby.system.home=/tmp/derby"
 
 # Entrypoint, for example notebook, pyspark or spark-sql
 if [[ $# -gt 0 ]] ; then
